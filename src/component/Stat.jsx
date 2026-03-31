54157ff27946d0b8713abadd2e262cf91525d0b1
Jony-Mia <ajonymia321@gmail.com>
2026-03-31 22:53:19 +0600

    testing

--- src/component/Stat.jsx
+++ src/component/Stat.jsx
@@ -2,27 +2,27 @@ import React from 'react';
 
 const Stat = () => {
     return (
-        
-        <div className='m-auto container grid grid-cols-3  justify-center gap-70'>
+
+        <div className='m-auto container grid grid-cols-3 py-10 text-white justify-center gap-70'>
             <div className="stats shadow">
                 <div className="stat">
-                    <div className="stat-title">Total Page Views</div>
-                    <div className="stat-value">89,400</div>
-                    <div className="stat-desc">21% more than last month</div>
+                    
+                    <div className="stat-value text-4xl">50k+</div>
+                    <div className="stat-desc text-2xl text-white">Active User</div>
                 </div>
             </div>
             <div className="stats shadow">
                 <div className="stat">
-                    <div className="stat-title">Total Page Views</div>
-                    <div className="stat-value">89,400</div>
-                    <div className="stat-desc">21% more than last month</div>
+                    
+                    <div className="stat-value text-4xl">200+</div>
+                    <div className="stat-desc text-2xl text-white">Premium Tools</div>
                 </div>
             </div>
             <div className="stats shadow">
                 <div className="stat">
-                    <div className="stat-title">Total Page Views</div>
-                    <div className="stat-value">89,400</div>
-                    <div className="stat-desc">21% more than last month</div>
+                    
+                    <div className="stat-value text-4xl">4.9</div>
+                    <div className="stat-desc text-2xl text-white">Ratings</div>
                 </div>
             </div>
         </div>
